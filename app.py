19f39b03695bb5ed05b1ac84543423495fa17dee
ÿiminchen1999 <yc4195@cumc.columbia.edu>
2023-03-02 13:54:45 -0500

    6

--- app.py
+++ app.py
@@ -17,15 +17,12 @@ st.title('Hello Pyvis')
 st.sidebar.title('Choose your week')
 option=st.sidebar.selectbox('select graph',('week1','week2', 'week3'))
 physics=st.sidebar.checkbox('add physics interactivity?')
-got.simple_func(physics)
 
 if option=='week1':
   HtmlFile = open("index.html", 'r', encoding='utf-8')
   source_code = HtmlFile.read() 
   components.html(source_code, height = 900,width=900)
 
-
-got.got_func(physics)
 
 if option=='week2':
   HtmlFile = open("index2.html", 'r', encoding='utf-8')
@@ -34,7 +31,6 @@ if option=='week2':
 
 
 
-got.karate_func(physics)
 
 if option=='week3':
   HtmlFile = open("index3.html", 'r', encoding='utf-8')
