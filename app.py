75401c8a0336720878c18c17011f201f34da5682
ÿiminchen1999 <yc4195@cumc.columbia.edu>
2023-03-02 13:53:51 -0500

    5

--- app.py
+++ app.py
@@ -14,21 +14,21 @@ st.title('Hello Pyvis')
 #  return html
 
 #Network._repr_html_ = net_repr_html
-st.sidebar.title('Choose your favorite Graph')
-option=st.sidebar.selectbox('select graph',('Simple','Karate', 'GOT'))
+st.sidebar.title('Choose your week')
+option=st.sidebar.selectbox('select graph',('week1','week2', 'week3'))
 physics=st.sidebar.checkbox('add physics interactivity?')
 got.simple_func(physics)
 
-if option=='Simple':
-  HtmlFile = open("test.html", 'r', encoding='utf-8')
+if option=='week1':
+  HtmlFile = open("index.html", 'r', encoding='utf-8')
   source_code = HtmlFile.read() 
   components.html(source_code, height = 900,width=900)
 
 
 got.got_func(physics)
 
-if option=='GOT':
-  HtmlFile = open("gameofthrones.html", 'r', encoding='utf-8')
+if option=='week2':
+  HtmlFile = open("index2.html", 'r', encoding='utf-8')
   source_code = HtmlFile.read() 
   components.html(source_code, height = 1200,width=1000)
 
@@ -36,7 +36,7 @@ if option=='GOT':
 
 got.karate_func(physics)
 
-if option=='Karate':
-  HtmlFile = open("karate.html", 'r', encoding='utf-8')
+if option=='week3':
+  HtmlFile = open("index3.html", 'r', encoding='utf-8')
   source_code = HtmlFile.read() 
   components.html(source_code, height = 1200,width=1000)
