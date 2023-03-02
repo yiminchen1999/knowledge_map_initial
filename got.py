56e84453b3dd4ba04a5be2ea1108ca6358225fdf
ÿiminchen1999 <yc4195@cumc.columbia.edu>
2023-03-02 14:19:54 -0500

    gotpy

--- got.py
+++ got.py
@@ -1,71 +1,117 @@
-import networkx as nx
+
 import matplotlib.pyplot as plt
+import streamlit as st
 from pyvis.network import Network
 import pandas as pd
-import streamlit as st
+import networkx as nx
+import webbrowser
+import os
+from IPython.core.display import display, HTML
 
+week_keyword = pd.read_excel("week_keyword_table_s01_2021.xlsx", index_col=0, engine='openpyxl')
+keyword_category = pd.read_excel("keyword_category_table.xlsx", index_col=0, engine='openpyxl')
+#print(week_keyword)
+#print(keyword_category)
+keywords = list(week_keyword.head())
+#print(keywords)
+# the number shows which category the keyword belongs to
+keywords_group = keyword_category.iloc[:, 0].to_numpy()
+#print(keywords_group)
+# extract each week's keyword list
+week1 = week_keyword.iloc[0].to_numpy()
+week2 = week_keyword.iloc[1].to_numpy()
+week3 = week_keyword.iloc[2].to_numpy()
+#print(week1)
+#print(week2)
+#print(week3)
+# create gradient color for each week's nodes
+from palettable.colorbrewer.sequential import Blues_3
+palette = Blues_3.hex_colors
+#print(palette)
+palette[0]
 
 def got_func(physics):
-  got_net = Network(height="600px", width="100%", font_color="black",heading='Game of Thrones Graph')
-
-# set the physics layout of the network
-  got_net.barnes_hut()
-  got_data = pd.read_csv("https://www.macalester.edu/~abeverid/data/stormofswords.csv")
-  #got_data = pd.read_csv("stormofswords.csv")
-  #got_data.rename(index={0: "Source", 1: "Target", 2: "Weight"}) 
-  sources = got_data['Source']
-  targets = got_data['Target']
-  weights = got_data['Weight']
-
-  edge_data = zip(sources, targets, weights)
-
-  for e in edge_data:
-    src = e[0]
-    dst = e[1]
-    w = e[2]
-
-    got_net.add_node(src, src, title=src)
-    got_net.add_node(dst, dst, title=dst)
-    got_net.add_edge(src, dst, value=w)
-
-  neighbor_map = got_net.get_adj_list()
-
-# add neighbor data to node hover data
-  for node in got_net.nodes:
-    node["title"] += " Neighbors:<br>" + "<br>".join(neighbor_map[node["id"]])
-    node["value"] = len(neighbor_map[node["id"]])
+  net1 = Network(notebook=False, heading="Individual Knowledge Map Week 1")
+  # name for html
+  title = []
+  html_name = 'index.html'
+  notebook = False
+  # cacluates the number of occurences for each keywork for week 1
+  occurence_week1 = week_keyword.iloc[:1].sum()
+  for i, value in enumerate(week1):
+    if value == 1:
+      net1.add_node(i, label=keywords[i], title='Week1', size=(int(occurence_week1[i]) + 1) * 3, color=palette[0])
+  # show the all node ids we have in the net
+  net1_id = [dic['id'] for dic in net1.nodes]
+  # print(net1_id)
+  for i in net1_id:
+    for j in net1_id:
+      if i != j and keywords_group[i] == keywords_group[j]:
+        net1.add_edge(i, j)
+  # net1.show("social_network.html")
   if physics:
-    got_net.show_buttons(filter_=['physics'])
-  got_net.show("gameofthrones.html")
+    net1.show_buttons(filter_=['physics'])
+  #net1.show_buttons(filter_="physics")
+  net1.show('index.html')
+
+
+
   
 
-def simple_func(physics): 
-  nx_graph = nx.cycle_graph(10)
-  nx_graph.nodes[1]['title'] = 'Number 1'
-  nx_graph.nodes[1]['group'] = 1
-  nx_graph.nodes[3]['title'] = 'I belong to a different group!'
-  nx_graph.nodes[3]['group'] = 10
-  nx_graph.add_node(20, size=20, title='couple', group=2)
-  nx_graph.add_node(21, size=15, title='couple', group=2)
-  nx_graph.add_edge(20, 21, weight=5)
-  nx_graph.add_node(25, size=25, label='lonely', title='lonely node', group=3)
-
-
-  nt = Network("500px", "500px",notebook=True,heading='')
-  nt.from_nx(nx_graph)
-  #physics=st.sidebar.checkbox('add physics interactivity?')
+def simple_func(physics):
+  net2 = Network(notebook=False, heading="Individual Knowledge Map Week 2")
+  # name for html
+  title = []
+  html_name = 'index2.html'
+  # net2 = Network(notebook=True, heading="Individual Knowledge Map Week 2", filter_menu=True, cdn_resources='remote')
+  # cacluates the number of occurences for each keywork for week 1 & week2 in total
+  occurence_week2 = week_keyword.iloc[:2].sum()
+  for i, value in enumerate(week1):
+    if value == 1:
+      net2.add_node(i, label=keywords[i], title='Week1', size=(int(occurence_week2[i]) + 1) * 3, color=palette[0])
+  for i, value in enumerate(week2):
+    if value == 1:
+      net2.add_node(i, label=keywords[i], title='Week2', size=(int(occurence_week2[i]) + 1) * 3, color=palette[1])
+  # show the all node ids we have in the net
+  net2_id = [dic['id'] for dic in net2.nodes]
+  print(net2_id)
+  for i in net2_id:
+    for j in net2_id:
+      if i != j and keywords_group[i] == keywords_group[j]:
+        net2.add_edge(i, j)
+  # net2.show("social_network.html")
+  # net2.show_buttons(filter_="physics")
   if physics:
-    nt.show_buttons(filter_=['physics'])
-  nt.show('test.html')
+    net2.show_buttons(filter_=['physics'])
+  net2.show('index2.html')
 
 
-def karate_func(physics): 
-  G = nx.karate_club_graph()
-
-
-  nt = Network("500px", "500px",notebook=True,heading='Zachary’s Karate Club graph')
-  nt.from_nx(G)
-  #physics=st.sidebar.checkbox('add physics interactivity?')
+def karate_func(physics):
+  net3 = Network(notebook=False, heading="Individual Knowledge Map Week 3")
+  title = []
+  html_name = 'index3.html'
+  # net3 = Network(notebook=True, heading="Individual Knowledge Map Week 3", filter_menu=True, cdn_resources='remote')
+  # cacluates the number of occurences for each keywork for week 1 & week2 & week3 in total
+  occurence_week3 = week_keyword.iloc[:2].sum()
+  for i, value in enumerate(week1):
+    if value == 1:
+      net3.add_node(i, label=keywords[i], title='Week1', size=(int(occurence_week3[i]) + 1) * 3, color=palette[0])
+  for i, value in enumerate(week2):
+    if value == 1:
+      net3.add_node(i, label=keywords[i], title='Week2', size=(int(occurence_week3[i]) + 1) * 3, color=palette[1])
+  for i, value in enumerate(week3):
+    if value == 1:
+      net3.add_node(i, label=keywords[i], title='Week3', size=(int(occurence_week3[i]) + 1) * 3, color=palette[2])
+  # show the all node ids we have in the net
+  net3_id = [dic['id'] for dic in net3.nodes]
+  print(net3_id)
+  for i in net3_id:
+    for j in net3_id:
+      if i != j and keywords_group[i] == keywords_group[j]:
+        net3.add_edge(i, j)
+  # net3.show("social_network.html")
+  #net3.show_buttons(filter_="physics")
   if physics:
-    nt.show_buttons(filter_=['physics'])
-  nt.show('karate.html')
+    net3.show_buttons(filter_=['physics'])
+  net3.show('index3.html')
+
